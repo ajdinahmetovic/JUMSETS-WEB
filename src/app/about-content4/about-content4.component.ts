82b4f4234136bba8225d6199a19e7994c605068c
ajdin <ahmetovicajdin@gmail.com>
2019-02-07 09:23:53 +0100

    A4C added

---
+++ src/app/about-content4/about-content4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-content4',
+  templateUrl: './about-content4.component.html',
+  styleUrls: ['./about-content4.component.scss']
+})
+export class AboutContent4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
